993bfb6504dd584f4692e801b63fd5a040675950
Erhan Koral <erhan.16k@gmail.com>
2024-02-14 16:26:44 +0100

    test added

--- index.test.js
+++ index.test.js
@@ -1,20 +1,18 @@
 import app from "./index";
 import request from "supertest";
 
-
 describe('POST /users', () => {
     it('should return HTTP success for valid request', async () => {
       const response = await request(app)
         .post('/users')
         .send({ "name": "exampleUser", "password": "password123" });
-      expect(response.status).toBe(201);
-      done();
+      expect(response.statusCode).toBe(201);
     });
   
     it('should return HTTP error for invalid request', async () => {
       const response = await request(app)
         .post('/users')
         .send({}); 
-      expect(response.status).toBe(400); 
+      expect(response.statusCode).toBe(400); 
     });
   });
